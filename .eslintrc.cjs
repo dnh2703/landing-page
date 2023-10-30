296b5631d4bbc6acb7daf6363d83eac763b2cc2e
dnh2703 <huydn@vmogroup.com>
2023-10-30 15:43:31 +0700

    [HuyDN]: initialize project

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb/typescript',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules'],
+  parser: '@typescript-eslint/parser',
+  parserOption: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react/react-in-jsx-scope': 0,
+  },
+}
---
+++ src/routes/router.paths.ts
@@ -0,0 +1,3 @@
+export const PATH_HOME = '/';
+export const PATH_USER_PROFILE = '/profile';
+export const PATH_CONTACT = '/contact';
